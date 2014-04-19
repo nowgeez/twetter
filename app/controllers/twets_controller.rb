1afad64e1282160475037f11550a9b3df4a165eb
Greg Nowicki <nowicki.greg@gmail.com>
2014-04-19 17:08:53 -0400

    reworked index view

--- app/controllers/twets_controller.rb
+++ app/controllers/twets_controller.rb
@@ -41,7 +41,13 @@ class TwetsController < ApplicationController
 
   # Sets the @twets instance variable to all twets viewable by the current user
   def get_twets
-    @twets = current_user.all_twets
+     # @twets = current_user.all_twets
+    if params[:username]
+      @user = User.where(:username => params[:username]).first
+      @twets = Twet.by_user_ids(@user.id) if @user
+    else
+      @twets = current_user.all_twets
+    end
   end
 
   # http://guides.rubyonrails.org/action_controller_overview.html#strong-parameters
