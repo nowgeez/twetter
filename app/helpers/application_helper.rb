bb5edec5c8cccb0336db6910b23fd251f1aeb41a
Greg Nowicki <nowicki.greg@gmail.com>
2014-04-22 22:00:43 -0400

    initiating gravatar feature branch

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,6 +19,13 @@ module ApplicationHelper
     end
   end
 
+  def avatar_url(user)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
+    "http://www.gravatar.com/avatar/#{gravatar_id}.png"
+
+  end
+
+
   # Generates a left navigation link setting the class according to the result of a
   # call to the #active_class method.
   #
